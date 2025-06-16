884da83839fa4f395c550203888882ad594e623f
jiwoojung <jungjiwoojjw@gmail.com>
2025-06-16 22:24:57 +0900

    feat: 인풋 컴포넌트 추가

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -13,13 +13,14 @@ const Main = () => {
             <Text.H1 fontWeight={600} mt="100" p={100} color={"blue"}>안녕하세요!!</Text.H1>
             <Text.Caption2 mt="50" fontWeight={900} color={"orange"}>헬로</Text.Caption2>
 
-            <BasicButton roundButton="large" variant="default" mb="20">
+            <BasicButton basicButton="small" variant="disabled" pt="20">
                 텍스트
-                <Icon icon={faCheck} ml="10" fontSize="10" />
             </BasicButton>
             <Text.Body2 color={"red"}>정지우입니다</Text.Body2>
-            <BasicInput type="text" placeholder="이름을 입력하세요"/>
+            <BasicInput type="text" placeholder="이름을 입력하세요" disabled/>
 
+
+            테스트 222
         </section>
     );
 };
